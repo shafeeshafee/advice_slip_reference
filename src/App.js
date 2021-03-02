8530a5678bdad85b550dae99cc0ad4a5341e929f
Shafee <shafeelinks@gmail.com>
2021-03-01 23:21:51 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Advice from "./components/Advice";
+import Nav from "./components/Nav";
 
 function App() {
 	return (
 		<div className="App">
+			<Nav />
 			<div className="header">
-				<h1>STOP. Get advice first!</h1>
+				<h1>Advice App</h1>
 			</div>
 
 			<Advice />
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+	return (
+		<div className="nav">
+			<h1>Logo</h1>
+			<ul>
+				<li>About</li>
+				<li>Team</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Nav;
